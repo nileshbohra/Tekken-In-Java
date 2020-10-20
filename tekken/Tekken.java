810c3e31e0793dc45190c99ca2cce6a978c08a5a
Nike2000 <nmbohra2001@gmail.com>
2020-10-20 12:36:54 +0530

    Main Class of Tekken

---
+++ tekken/Tekken.java
@@ -0,0 +1,16 @@
+package tekken;
+
+public class Tekken {
+	
+	//main method
+	public static void main(String[] args) {
+		//objects
+		MainScreen m = new MainScreen();
+		//Character chr = new Character();
+		
+		//method call
+		m.start();
+		m.GameModes();
+		m.ModeSelect();	
+	}
+}
